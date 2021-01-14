dba61cd6ec12d0b22836ab38af861c7cdb5ce5ef
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-14 19:17:07 -0300

    starting

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
 <?php require 'header.php' ?>
   <section class="full-page">
-  <div class="slider-container">
-      <ul class="rslides">
-        <li>
-          <video autoplay loop muted playsinline>
-          <source src="https://de0dqb7pdorb1.cloudfront.net/611_rain_www.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-          </video></li>
-        <li>
-          <video autoplay loop muted playsinline>
-          <source src="https://de0dqb7pdorb1.cloudfront.net/cameraY_cinema_www2.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-          </video></li>
-        </li>
-        <li>
-          <video width="200%" autoplay loop muted playsinline>
-          <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-          </video></li>
-        </li>       
-      </ul>
+  <div class="slider-container">      
+          <video autoplay loop muted playsinline id="video1" class="video1">
+            <source src="https://de0dqb7pdorb1.cloudfront.net/611_rain_www.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+          </video>
+
+          <video loop muted playsinline id="video2" class="video2">
+            <source src="https://de0dqb7pdorb1.cloudfront.net/cameraY_cinema_www2.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+          </video>
+        
+          <video loop muted playsinline id="video3" class="video3">
+            <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+          </video>
+    </div>           
       <img class="logotipo" src="./img/avatar_bvd.png"> 
-    </div>
+    
 
   </section>
   <section class="holder">      
