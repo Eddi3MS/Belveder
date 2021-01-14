97d33b8e877d1c9315cffbde22e64f79c56107aa
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-14 07:12:42 -0300

    starting project

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    <div class="logo"></div>
+    <nav class="navigation">
+      <ul>
+        <li><a>PROJETOS</a></li>
+        <li><a>SERVIÇOS</a></li>
+        <li><a>SOBRE</a></li>
+        <li><a>CONTATO</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <ul class="rslides">
+      <li><img src="1.jpg" alt=""></li>
+      <li><img src="2.jpg" alt=""></li>
+      <li><img src="3.jpg" alt=""></li>
+    </ul>
+  </main>
+  <footer>
+    <div class="logo-footer"></div>
+    <div class="icones-footer"></div>
+  </footer> 
+</body>
+</html>
