454363425085909126529f2df785a5104ac3e296
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-14 08:27:54 -0300

    starting

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <?php require 'header.php' ?>
   
   <main>
-    <ul class="rslides">
-      <li>
-        <video width="100%" autoplay loop muted playsinline>
-				<source src="./img/video.mp4" type="video/mp4">
-			    Your browser does not support the video tag.
-			  </video></li>
-      <li>
-        <video width="100%" autoplay loop muted playsinline>
-				<source src="./img/video.mp4" type="video/mp4">
-			    Your browser does not support the video tag.
-			  </video></li>
-      </li>
-      <li>
-        <video width="100%" autoplay loop muted playsinline>
-				<source src="./img/video.mp4" type="video/mp4">
-			    Your browser does not support the video tag.
-			  </video></li>
-      </li>
-    </ul>
+    <div class="slider-container">
+      <ul class="rslides">
+        <li>
+          <video width="100%" autoplay loop muted playsinline>
+          <source src="./img/video.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+          </video></li>
+        <li>
+          <video width="100%" autoplay loop muted playsinline>
+          <source src="./img/video.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+          </video></li>
+        </li>
+        <li>
+          <video width="100%" autoplay loop muted playsinline>
+          <source src="./img/video.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+          </video></li>
+        </li>
+      </ul>
+    </div>
     <img class="logotipo" src="./img/avatar_bvd.png">
   </main>
 
