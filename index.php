ad4214c4003649bb52af37bc451f1c638ad09993
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-14 18:28:11 -0300

    starting

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
   <div class="slider-container">
       <ul class="rslides">
         <li>
-          <video width="100%" autoplay loop muted playsinline>
+          <video autoplay loop muted playsinline>
           <source src="https://de0dqb7pdorb1.cloudfront.net/611_rain_www.mp4" type="video/mp4">
             Your browser does not support the video tag.
           </video></li>
         <li>
-          <video width="100%" autoplay loop muted playsinline>
+          <video autoplay loop muted playsinline>
           <source src="https://de0dqb7pdorb1.cloudfront.net/cameraY_cinema_www2.mp4" type="video/mp4">
             Your browser does not support the video tag.
           </video></li>
         </li>
         <li>
-          <video width="100%" autoplay loop muted playsinline>
+          <video autoplay loop muted playsinline>
           <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
             Your browser does not support the video tag.
           </video></li>
