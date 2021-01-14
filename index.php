23db8f3292724914a71ab5279b17959346204721
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-14 19:19:15 -0300

    starting

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php require 'header.php' ?>
   <section class="full-page">
   <div class="slider-container">      
-          <video autoplay loop muted playsinline id="video1" class="video1">
+          <video autoplay muted playsinline id="video1" class="video1">
             <source src="https://de0dqb7pdorb1.cloudfront.net/611_rain_www.mp4" type="video/mp4">
               Your browser does not support the video tag.
           </video>
 
-          <video loop muted playsinline id="video2" class="video2">
+          <video muted playsinline id="video2" class="video2">
             <source src="https://de0dqb7pdorb1.cloudfront.net/cameraY_cinema_www2.mp4" type="video/mp4">
               Your browser does not support the video tag.
           </video>
         
-          <video loop muted playsinline id="video3" class="video3">
+          <video muted playsinline id="video3" class="video3">
             <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
               Your browser does not support the video tag.
           </video>
