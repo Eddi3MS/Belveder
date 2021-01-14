b5f7bdddc2993970eb516c6fd664b3a4a4241804
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-14 20:36:05 -0300

    starting

--- contato.php
+++ contato.php
@@ -1,4 +1,6 @@
 <?php require 'header.php' ?>
-<section class="bg-contato"></section>
+<section class="bg-contato">
+  <img class="logotipo" src="./img/avatar_bvd.png">
+</section>
 <section class="holder"></section>
 <?php require 'footer.php' ?>
