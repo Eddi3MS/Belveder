23b6517e75eab2d85c09df25f2926535242a0967
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-14 07:28:19 -0300

    starting

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer class="footer">
+    <div class="container  informacao-footer">
+        <h3>Contato</h3>
+        <p><i class="fas fa-envelope"></i>contato.eddi3@hotmail.com</p>
+        <p><i class="fas fa-phone-square-alt"></i>+55 37 99985-3557</p>
+        <p><i class="fas fa-map-marker-alt"></i>Minas Gerais - Brazil</p>
+    </div>
+    <div class="divisao"></div>
+    <div class="container  copyright">
+        <p>Copyright © <?php echo date('Y') ?> Eddi3 Web Developer | Todos os direitos reservados.</p>
+    </div>
+    <!--container-->
+</footer>
+<!--footer-->
+<!-- JavaScript -->
+<script src="js/libs/jquery-3.5.1.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+<!-- JavaScript -->
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <header>
-    <div class="logo"></div>
-    <nav class="navigation">
-      <ul>
-        <li><a>PROJETOS</a></li>
-        <li><a>SERVIÇOS</a></li>
-        <li><a>SOBRE</a></li>
-        <li><a>CONTATO</a></li>
-      </ul>
-    </nav>
-  </header>
+<?php require 'footer.php' ?>
+  
   <main>
     <ul class="rslides">
       <li><img src="1.jpg" alt=""></li>
@@ -24,9 +7,5 @@
       <li><img src="3.jpg" alt=""></li>
     </ul>
   </main>
-  <footer>
-    <div class="logo-footer"></div>
-    <div class="icones-footer"></div>
-  </footer> 
-</body>
-</html>
+
+<?php require 'footer.php' ?>
