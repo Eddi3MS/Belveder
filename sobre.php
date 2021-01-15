df824a4f9c2872c2a54076bb25c7cb0cc0780826
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-15 11:14:26 -0300

    starting

---
+++ sobre.php
@@ -0,0 +1,25 @@
+<?php require 'header.php' ?>
+
+<section class="sobre">
+  <div class="sobre-nos">
+  <h1>SOBRE NÓS</h1>
+  <p>Lorem ipsum, dolor sit amet consectetur 
+  adipisicing elit. Exercitationem sed vitae 
+  odit minus perferendis iure voluptas 
+  necessitatibus tenetur eveniet. Quod 
+  repellendus sed nesciunt asperiores ipsam 
+  reiciendis, tempora placeat voluptas totam.</p>
+  <p>Lorem ipsum dolor sit amet consectetur 
+  adipisicing elit. Nemo, molestiae!</p>
+  </div>
+  <div class="logo-sobre">
+  <img src="./img/avatar_black.png" />
+  </div>  
+</section>
+
+  <div class="membro1"></div>
+  <div class="membro2"></div>
+  <div class="membro3"></div>
+  <div class="membro4"></div>
+
+<?php require 'footer.php' ?>
