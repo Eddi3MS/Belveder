dffb381a85dcc41ec770ef1d81e0976d68118453
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-15 13:16:54 -0300

    starting

--- sobre.php
+++ sobre.php
@@ -23,16 +23,20 @@
 </section>
 <section class="membros">
   <div class="membro1">
-    <p>Nome Sobrenome</p> <p>Cargo</p>
+    <p>Nome Sobrenome</p>
+    <p>Cargo</p>
   </div>
   <div class="membro2">
-  <p>Nome Sobrenome</p> <p>Cargo</p>
+  <p>Nome Sobrenome</p>
+  <p>Cargo</p>
   </div>
   <div class="membro3">
-  <p>Nome Sobrenome</p> <p>Cargo</p>
+  <p>Nome Sobrenome</p>
+  <p>Cargo</p>
   </div>
   <div class="membro4">
-  <p>Nome Sobrenome</p> <p>Cargo</p>
+  <p>Nome Sobrenome</p>
+  <p>Cargo</p>
   </div>
 </section>
 <?php require 'footer.php' ?>
