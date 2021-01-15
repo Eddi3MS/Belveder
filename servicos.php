1838f5a08ef1e61bdbe3f776e420854f70932d47
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-15 19:40:18 -0300

    starting

---
+++ servicos.php
@@ -0,0 +1,39 @@
+<?php require 'header.php' ?>
+<section class="shadow"></section>
+<section class="servicos">
+  <div class="titulo-serv">
+    <h1>VISUALIZAÇÃO ARQUITETÔNICA</h1>
+  </div>
+  <div class="titulo-desc">
+    <p>
+      Lorem ipsum dolor sit amet consectetur 
+      adipisicing elit. Eligendi dolorem, sequi 
+      quis, quaerat perferendis reiciendis minima 
+      culpa accusamus consequatur debitis placeat 
+      laudantium quas iusto fugiat nisi ratione non 
+      aperiam ducimus.
+    </p>
+  </div>
+  <div class="nav-serv">
+    <nav>
+      <ul>
+        <li><a href="#">Ilustrações 3D</a></li>
+        <li><a href="#">Animações</a></li>
+        <li><a href="#">Plantas Humanizadas</a></li>
+        <li><a href="#">Realidade Virtual</a></li>
+      </ul>
+    </nav>
+  </div>
+  <div class="banner"></div>
+  <div class="ilustracoes"></div>
+  <div class="ilustracoes-img"></div>
+  <div class="animacoes"></div>
+  <div class="animacoes-img"></div>
+  <div class="plantas"></div>
+  <div class="plantas-img"></div>
+  <div class="realidade-virtual"></div>
+  <div class="realidade-virtual-img"></div>
+</section>
+
+
+<?php require 'footer.php' ?>
