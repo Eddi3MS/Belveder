7e2c7a883e0bb822a608270c91adb1ffca0a2ac3
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-14 21:29:32 -0300

    starting

--- contato.php
+++ contato.php
@@ -1,6 +1,18 @@
 <?php require 'header.php' ?>
 <section class="bg-contato">
   <img class="logotipo-contato" src="./img/avatar_bvd.png">
+  <div class="contatos">
+    <p>contato@blvdr.com.br</p>
+    <p>Rio de Janeiro, Brasil</p>
+    <p>+55(21)965590857</p>
+    <div class="icones-footer1">
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-linkedin-in"></i>
+            <i class="fab fa-whatsapp"></i>
+            <i class="fab fa-behance"></i>
+        </div>
+  </div>
 </section>
 <section class="holder"></section>
 <?php require 'footer.php' ?>
