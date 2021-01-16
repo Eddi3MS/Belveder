606ca69de4e8dbe212a4de57f1112b1b75f98e75
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-16 15:59:50 -0300

    starting

--- projetos.php
+++ projetos.php
@@ -1,131 +1,169 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!--Tags para SEO-->
-    <meta
-      name="description"
-      content="---"
-    />
-    <meta
-      name="keywords"
-      content="--"
-    />
-    <meta name="author" content="Dev Eddi3" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <script
-      src="https://kit.fontawesome.com/8e3d354095.js"
-      crossorigin="anonymous"
-    ></script>
-    <link rel="shortcut icon" type="image-x/png" href="img/icon.ico" />
-    <title>Dev Eddi3 - Belveder</title>
-  </head>
-  <body>
-    <header class="header-black">
-      <div class="logo-header"><a href="/"><img src="./img/logo.png"/></a></div>
-      <div class="nav-container">
-        <nav class="navigation">
-          <ul>
-            <li><a title="Projetos" href="/projetos">Projetos</a></li>
-             <li>
-               <a title="Serviços" href="/servicos">Serviços</a>
-             </li>
-             <li>
-               <a title="Sobre" href="/sobre">Sobre</a>
-             </li>
-             <li>
-               <a title="Contato" href="/contato">Contato</a>
-             </li>
-          </ul>
-        </nav>
-        <!--menu-desktop-->
-        <div class="menu" id="menu">
-          <div class="bar"></div>
-        </div>
 
-        <nav class="nav-mobile">
-          <ul>
-             <li>
-              <a title="Projetos" href="/projetos">Projetos</a>
-             </li>
-             <li>
-               <a title="Serviços" href="/serviços">Serviços</a>
-             </li>
-             <li>
-               <a title="Sobre" href="/sobre">Sobre</a>
-             </li>
-             <li>
-               <a title="Contato" href="/contato">Contato</a>
-             </li>
-          </ul>
-        </nav>
-        <!--menu-mobile-->
-        </div><!-- nav-container -->
-    </header>
-    <!--header-->
-<section class="principais">
-  <div class="slider-projetos">
-    <video autoplay muted playsinline id="projeto1" class="video-projeto  projeto1">
-      <source src="https://de0dqb7pdorb1.cloudfront.net/611_rain_www.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!--Tags para SEO-->
+  <meta name="description" content="---" />
+  <meta name="keywords" content="--" />
+  <meta name="author" content="Dev Eddi3" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <script src="https://kit.fontawesome.com/8e3d354095.js" crossorigin="anonymous"></script>
+  <link rel="shortcut icon" type="image-x/png" href="img/icon.ico" />
+  <title>Dev Eddi3 - Belveder</title>
+</head>
 
-    <video muted playsinline id="projeto2" class="video-projeto  projeto2">
-      <source src="https://de0dqb7pdorb1.cloudfront.net/cameraY_cinema_www2.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
+<body>
+  <header class="header-black">
+    <div class="logo-header"><a href="/"><img src="./img/logo.png" /></a></div>
+    <div class="nav-container">
+      <nav class="navigation">
+        <ul>
+          <li><a title="Projetos" href="/projetos">Projetos</a></li>
+          <li>
+            <a title="Serviços" href="/servicos">Serviços</a>
+          </li>
+          <li>
+            <a title="Sobre" href="/sobre">Sobre</a>
+          </li>
+          <li>
+            <a title="Contato" href="/contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+      <!--menu-desktop-->
+      <div class="menu" id="menu">
+        <div class="bar"></div>
+      </div>
 
-    <video muted playsinline id="projeto3" class="video-projeto  projeto3">
-      <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
-  </div>
-</section>
-<section class="seletor">
-  <div class="seletor-content">
-    <img style="width:30px; height: 30px;" src="img/avatar_black.png" alt="logo" />
-    <i class="fas fa-chair"></i>
-    <i class="far fa-building"></i>
-    <i class="fab fa-youtube"></i>
-  </div>
-</section>
-<section class="galeria">
-<div class="i"></div>
-<div class="i"></div>
-<div class="i"></div>
-<div class="i"></div>
-<div class="i"></div>
-<div class="i"></div>
-<div class="i"></div>
-<div class="i"></div>
-<div class="i"></div>
+      <nav class="nav-mobile">
+        <ul>
+          <li>
+            <a title="Projetos" href="/projetos">Projetos</a>
+          </li>
+          <li>
+            <a title="Serviços" href="/serviços">Serviços</a>
+          </li>
+          <li>
+            <a title="Sobre" href="/sobre">Sobre</a>
+          </li>
+          <li>
+            <a title="Contato" href="/contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+      <!--menu-mobile-->
+    </div><!-- nav-container -->
+  </header>
+  <!--header-->
+  <section class="principais">
+    <div class="slider-projetos">
+      <video autoplay muted playsinline id="projeto1" class="video-projeto  projeto1">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/611_rain_www.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
 
-</section>
+      <video muted playsinline id="projeto2" class="video-projeto  projeto2">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/cameraY_cinema_www2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
 
-<footer class="footer">
-  <div class="logo-footer">
-    <img src="./img/logo1.png" />
-  </div>
-  <div class="icones-footer">
-    <div class="icones-footer1">
-      <i class="fab fa-instagram"></i>
-      <i class="fab fa-facebook-f"></i>
-      <i class="fab fa-linkedin-in"></i>
-      <i class="fab fa-whatsapp"></i>
-      <i class="fab fa-behance"></i>
+      <video muted playsinline id="projeto3" class="video-projeto  projeto3">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
     </div>
-    <div class="icones-footer2">
-      <p>contato@blvdr.com.br</p>
+  </section>
+  <section class="seletor">
+    <div class="seletor-content">
+      <img style="width:30px; height: 30px;" src="img/avatar_black.png" alt="logo" />
+      <i class="fas fa-chair"></i>
+      <i class="far fa-building"></i>
+      <i class="fab fa-youtube"></i>
     </div>
-  </div>
-  <!--container-->
-</footer>
-<!--footer-->
-<!-- JavaScript -->
-<script src="js/libs/jquery-3.5.1.min.js"></script>
-<script src="js/project-loader.js"></script>
-<!-- JavaScript -->
+  </section>
+  <section class="galeria">
+    <div class="i">
+      <video muted playsinline id="galeria-videos" class="galeria-videos">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div class="i">
+      <video muted playsinline id="galeria-videos" class="galeria-videos">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div class="i">
+      <video muted playsinline id="galeria-videos" class="galeria-videos">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div class="i">
+      <video muted playsinline id="galeria-videos" class="galeria-videos">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div class="i">
+      <video muted playsinline id="galeria-videos" class="galeria-videos">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div class="i">
+      <video muted playsinline id="galeria-videos" class="galeria-videos">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div class="i">
+      <video muted playsinline id="galeria-videos" class="galeria-videos">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div class="i">
+      <video muted playsinline id="galeria-videos" class="galeria-videos">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div class="i">
+      <video muted playsinline id="galeria-videos" class="galeria-videos">
+        <source src="https://de0dqb7pdorb1.cloudfront.net/111_north2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+
+  </section>
+
+  <footer class="footer">
+    <div class="logo-footer">
+      <img src="./img/logo1.png" />
+    </div>
+    <div class="icones-footer">
+      <div class="icones-footer1">
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-facebook-f"></i>
+        <i class="fab fa-linkedin-in"></i>
+        <i class="fab fa-whatsapp"></i>
+        <i class="fab fa-behance"></i>
+      </div>
+      <div class="icones-footer2">
+        <p>contato@blvdr.com.br</p>
+      </div>
+    </div>
+    <!--container-->
+  </footer>
+  <!--footer-->
+  <!-- JavaScript -->
+  <script src="js/libs/jquery-3.5.1.min.js"></script>
+  <script src="js/project-loader.js"></script>
+  <!-- JavaScript -->
 </body>
 
 </html>
