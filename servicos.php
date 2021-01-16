2fc54df8348b01f5b3bc793b44a188aae6e9087c
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-16 15:50:56 -0300

    starting

--- servicos.php
+++ servicos.php
@@ -1,5 +1,69 @@
-<?php require 'header.php' ?>
-<section class="shadow"></section>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--Tags para SEO-->
+    <meta
+      name="description"
+      content="---"
+    />
+    <meta
+      name="keywords"
+      content="--"
+    />
+    <meta name="author" content="Dev Eddi3" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script
+      src="https://kit.fontawesome.com/8e3d354095.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="shortcut icon" type="image-x/png" href="img/icon.ico" />
+    <title>Dev Eddi3 - Belveder</title>
+  </head>
+  <body>
+    <header class="header-black">
+      <div class="logo-header"><a href="/"><img src="./img/logo.png"/></a></div>
+      <div class="nav-container">
+        <nav class="navigation">
+          <ul>
+            <li><a title="Projetos" href="/projetos">Projetos</a></li>
+             <li>
+               <a title="Serviços" href="/servicos">Serviços</a>
+             </li>
+             <li>
+               <a title="Sobre" href="/sobre">Sobre</a>
+             </li>
+             <li>
+               <a title="Contato" href="/contato">Contato</a>
+             </li>
+          </ul>
+        </nav>
+        <!--menu-desktop-->
+        <div class="menu" id="menu">
+          <div class="bar"></div>
+        </div>
+
+        <nav class="nav-mobile">
+          <ul>
+             <li>
+              <a title="Projetos" href="/projetos">Projetos</a>
+             </li>
+             <li>
+               <a title="Serviços" href="/serviços">Serviços</a>
+             </li>
+             <li>
+               <a title="Sobre" href="/sobre">Sobre</a>
+             </li>
+             <li>
+               <a title="Contato" href="/contato">Contato</a>
+             </li>
+          </ul>
+        </nav>
+        <!--menu-mobile-->
+        </div><!-- nav-container -->
+    </header>
+    <!--header-->
 <section class="servicos">
   <div class="titulo-serv">
     <h1>VISUALIZAÇÃO <br/>ARQUITETÔNICA</h1>
