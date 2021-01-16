c6906c07ca9045f00d1cd6ffcc40d97f760897a7
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-16 20:16:47 -0300

    starting

--- index.php
+++ index.php
@@ -1,4 +1,72 @@
-<?php require 'header.php' ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--Tags para SEO-->
+    <meta
+      name="description"
+      content="---"
+    />
+    <meta
+      name="keywords"
+      content="--"
+    />
+    <meta name="author" content="Dev Eddi3" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script
+      src="https://kit.fontawesome.com/8e3d354095.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="shortcut icon" type="image-x/png" href="img/icon.ico" />
+    <title>Dev Eddi3 - Belveder</title>
+  </head>
+  <body>
+    <div class="loader">
+      <img src="img/avatar_bvd.png" />
+    </div>
+    <header class="header">
+      <div class="logo-header"><a href="/"><img src="./img/logo1.png"/></a></div>
+      <div class="nav-container">
+        <nav class="navigation">
+          <ul>
+            <li><a title="Projetos" href="/projetos">Projetos</a></li>
+             <li>
+               <a title="Serviços" href="/servicos">Serviços</a>
+             </li>
+             <li>
+               <a title="Sobre" href="/sobre">Sobre</a>
+             </li>
+             <li>
+               <a title="Contato" href="/contato">Contato</a>
+             </li>
+          </ul>
+        </nav>
+        <!--menu-desktop-->
+        <div class="menu" id="menu">
+          <div class="bar"></div>
+        </div>
+
+        <nav class="nav-mobile">
+          <ul>
+             <li>
+              <a title="Projetos" href="/projetos">Projetos</a>
+             </li>
+             <li>
+               <a title="Serviços" href="/serviços">Serviços</a>
+             </li>
+             <li>
+               <a title="Sobre" href="/sobre">Sobre</a>
+             </li>
+             <li>
+               <a title="Contato" href="/contato">Contato</a>
+             </li>
+          </ul>
+        </nav>
+        <!--menu-mobile-->
+        </div><!-- nav-container -->
+    </header>
+    <!--header-->
   <section class="full-page">
   <div class="slider-container">      
           <video autoplay muted playsinline id="video1" class="index-videos  video1">
