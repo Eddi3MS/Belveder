9bdf6e452070dd73d9ae9a8371f40a262c900331
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-16 21:38:39 -0300

    nothing

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
   <section class="full-page">
     <div class="slider-container">
       <video autoplay muted playsinline id="video1" class="index-videos  video1">
-        <source src="https://www.youtube.com/embed/ABzDOSQkhTM" type="video/mp4">
+        <source src="./img/video1.mp4" type="video/mp4">
         Your browser does not support the video tag.
       </video>
 
