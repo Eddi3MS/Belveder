d7a8388431cdc44bb056b22c4294b302bdcad98c
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-23 12:50:32 -0300

    proj

---
+++ projetoX.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Projeto XXX</title>
+</head>
+<style>
+  section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    background-color: #f0f0f0;
+  }
+  h1 {
+    font-size: 25px;
+    color: red;
+  }
+</style>
+<body>
+  <section>
+    <h1> Nothing here yet.</h1>
+    <h1> Nada aqui ainda.</h1>
+  </section>
+  
+</body>
+</html>
