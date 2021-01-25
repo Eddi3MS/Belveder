cb59baf114ea956e798e8359e505dfec436a4f69
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-24 22:19:49 -0300

    proj

--- projetos.php
+++ projetos.php
@@ -100,7 +100,7 @@
         </div>
       </a>
     </div>
-    <div class="i" wm-city="nuvens">
+    <div class="i" wm-city="cinema">
       <video muted loop autoplay playsinline id="galeria-videos" class="galeria-videos">
         <source src="https://de0dqb7pdorb1.cloudfront.net/cameraY_cinema_www2.mp4" type="video/mp4">
         Your browser does not support the video tag.
@@ -114,7 +114,7 @@
         </div>
       </a>
     </div>
-    <div class="i" wm-city="nuvens">
+    <div class="i" wm-city="rain">
       <video muted loop autoplay playsinline id="galeria-videos" class="galeria-videos">
         <source src="https://de0dqb7pdorb1.cloudfront.net/611_rain_www.mp4" type="video/mp4">
         Your browser does not support the video tag.
@@ -128,7 +128,7 @@
         </div>
       </a>
     </div>
-    <div class="i" wm-city="nuvens">
+    <div class="i" wm-city="rain">
       <video muted loop autoplay playsinline id="galeria-videos" class="galeria-videos">
         <source src="https://de0dqb7pdorb1.cloudfront.net/611_rain_www.mp4" type="video/mp4">
         Your browser does not support the video tag.
@@ -156,7 +156,7 @@
         </div>
       </a>
     </div>
-    <div class="i" wm-city="nuvens">
+    <div class="i" wm-city="cinema">
       <video muted loop autoplay playsinline id="galeria-videos" class="galeria-videos">
         <source src="https://de0dqb7pdorb1.cloudfront.net/cameraY_cinema_www2.mp4" type="video/mp4">
         Your browser does not support the video tag.
@@ -170,7 +170,7 @@
         </div>
       </a>
     </div>
-    <div class="i" wm-city="nuvens">
+    <div class="i" wm-city="cinema">
       <video muted loop autoplay playsinline id="galeria-videos" class="galeria-videos">
         <source src="https://de0dqb7pdorb1.cloudfront.net/cameraY_cinema_www2.mp4" type="video/mp4">
         Your browser does not support the video tag.
@@ -184,7 +184,7 @@
         </div>
       </a>
     </div>
-    <div class="i" wm-city="nuvens">
+    <div class="i" wm-city="rain">
       <video muted loop autoplay playsinline id="galeria-videos" class="galeria-videos">
         <source src="https://de0dqb7pdorb1.cloudfront.net/611_rain_www.mp4" type="video/mp4">
         Your browser does not support the video tag.
