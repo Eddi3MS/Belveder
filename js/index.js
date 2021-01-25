a9138fcc7bb3ce4d7b636e8f82981ede6a5e48fc
Eddi3MS <edsonmarc2014@gmail.com>
2021-01-25 16:48:35 -0300

    proj

--- js/index.js
+++ js/index.js
@@ -27,26 +27,6 @@ video4.onended = function () {
   video1.style.opacity = 1;
 };
 
-
-// for <video id="bgvid">
-let vid = document.querySelectorAll("video");
-let playButton = document.querySelector("#PlayPause");
-
-
-playButton.addEventListener("click", function() {
-  if (vid.paused) {
-    vid.play();
-	playButton.classList.remove("play-video-button");
-    playButton.classList.add("pause-video-button");
-  } else {
-    vid.pause();
-	playButton.classList.add("play-video-button");
-    playButton.classList.remove("pause-video-button");
-  }
-});
-
-
-
 // pre-load
 
 window.addEventListener("load", function () {
